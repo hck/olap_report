777ede1fba3b7775f5635768bfc5d943739ff5cc
Alexander Svirin <alexsvirin@gmail.com>
2015-11-25 14:57:56 +0100

    Refactor specs for Measure::Statement.

--- spec/olap_report/cube/measure/statement_spec.rb
+++ spec/olap_report/cube/measure/statement_spec.rb
@@ -1,12 +1,12 @@
-require "spec_helper"
+require 'spec_helper'
 
-describe OlapReport::Cube::Measure::Statement do
+RSpec.describe OlapReport::Cube::Measure::Statement do
   [:+, :-, :*, :/].each do |func|
     describe "##{func}" do
-      it "#{func} should return proper sql select statements" do
-        m1 = described_class.new("SUM(a)")
-        m2 = described_class.new("COUNT(a)")
-        m2.public_send(func, m1).sql.should == described_class.new("COUNT(a) #{func} SUM(a)").sql
+      it "#{func} returns proper sql select statements" do
+        m1 = described_class.new('SUM(a)')
+        m2 = described_class.new('COUNT(a)')
+        expect(m2.public_send(func, m1).sql).to eq(described_class.new("COUNT(a) #{func} SUM(a)").sql)
       end
     end
   end
--- spec/olap_report/cube/query_methods_spec.rb
+++ spec/olap_report/cube/query_methods_spec.rb
@@ -7,29 +7,29 @@ describe OlapReport::Cube::QueryMethods do
 
   describe "::slice" do
     it "should fetch level" do
-      Fact.slice(dimensions: {user: :user_id}).should == Fact.select('"facts"."user_id" AS "user_id"').group('"facts"."user_id"')
+      Fact.slice(dimensions: { user: :user_id }).should == Fact.select('"facts"."user_id" AS "user_id"').group('"facts"."user_id"')
     end
 
     it "should fetch level with joins" do
-      Fact.slice(dimensions: {user: :group_id}).should == Fact.select('"users"."group_id" AS "group_id"').joins(:user).group('"users"."group_id"')
+      Fact.slice(dimensions: { user: :group_id }).should == Fact.select('"users"."group_id" AS "group_id"').joins(:user).group('"users"."group_id"')
     end
 
     it "should fetch level through another level" do
-      Fact.slice(dimensions: {user: :category}, skip_aggregated: true).should == Fact.select('"groups"."category" AS "category"').joins(user: :group).group('"groups"."category"')
+      Fact.slice(dimensions: { user: :category }, skip_aggregated: true).should == Fact.select('"groups"."category" AS "category"').joins(user: :group).group('"groups"."category"')
     end
 
     it "should fetch specified dimension & measure" do
       expected = Fact.select("#{Fact.quote_table_column('group_id', 'users')}, SUM(#{Fact.quote_table_column('score')}) group_score, COUNT(#{Fact.quote_table_column('score')}) score_count").
         joins(:user).group(Fact.quote_table_column('group_id', 'users'))
-      Fact.slice(dimensions: {user: :group_id}, measures: [:score_sum]).map(&:score_sum).should == expected.map(&:group_score)
-      Fact.slice(dimensions: {user: :group_id}, measures: [:score_sum]).map(&:group_id).should == expected.map(&:group_id)
-      Fact.slice(dimensions: {user: :group_id}, measures: [:score_count]).map(&:score_count).should == expected.map(&:score_count)
+      Fact.slice(dimensions: { user: :group_id }, measures: [:score_sum]).map(&:score_sum).should == expected.map(&:group_score)
+      Fact.slice(dimensions: { user: :group_id }, measures: [:score_sum]).map(&:group_id).should == expected.map(&:group_id)
+      Fact.slice(dimensions: { user: :group_id }, measures: [:score_count]).map(&:score_count).should == expected.map(&:score_count)
     end
 
     it "should calculate correct average" do
       expected = Fact.select("#{Fact.quote_table_column('group_id', 'users')}, AVG(#{Fact.quote_table_column('score')}) score_avg").
         joins(:user).group(Fact.quote_table_column('group_id', 'users'))
-      Fact.slice(dimensions: {user: :group_id}, measures: [:score_avg]).map(&:score_avg).should == expected.map(&:score_avg)
+      Fact.slice(dimensions: { user: :group_id }, measures: [:score_avg]).map(&:score_avg).should == expected.map(&:score_avg)
     end
 
     #it "should select data from aggregated table if it was defined for specified dimensions & levels" do
@@ -45,7 +45,7 @@ describe OlapReport::Cube::QueryMethods do
         field = '"facts"."created_at"'
 
         Fact.adapter.should_receive(:column_name).with(field, level.type).exactly(2).times
-        Fact.slice(dimensions: {date: :month, user: :user_id})
+        Fact.slice(dimensions: { date: :month, user: :user_id })
       end
     end
   end
@@ -56,7 +56,7 @@ describe OlapReport::Cube::QueryMethods do
         joins(user: :group).
         group('"users"."group_id"')
 
-      Fact.drilldown(dimensions: {user: :category}, measures: :score_count).should == expected
+      Fact.drilldown(dimensions: { user: :category }, measures: :score_count).should == expected
     end
 
     it "should fetch level details down the hierarchy to the specified level" do
@@ -64,7 +64,7 @@ describe OlapReport::Cube::QueryMethods do
         joins(user: :group).
         group('"facts"."user_id"')
 
-      Fact.drilldown(dimensions: {user: {category: :user_id}}, measures: :score_count).should == expected
+      Fact.drilldown(dimensions: { user: { category: :user_id } }, measures: :score_count).should == expected
     end
   end
 end
