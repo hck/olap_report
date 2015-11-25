11ee9557354fcf0bb4d8dbdb9d3f2426bae9557d
Alexander Svirin <alexsvirin@gmail.com>
2015-11-25 15:00:49 +0100

    Refactor specs for AbstractAdapter.

--- spec/olap_report/cube/adapters/abstract_adapter_spec.rb
+++ spec/olap_report/cube/adapters/abstract_adapter_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
-describe OlapReport::Cube::Adapters::AbstractAdapter do
-  describe "#initialize" do
-    it "should initialize instance with specified model" do
-      described_class.new(Fact).should be_instance_of(described_class)
+RSpec.describe OlapReport::Cube::Adapters::AbstractAdapter do
+  describe '#initialize' do
+    it 'initializes instance with specified model' do
+      expect(described_class.new(Fact)).to be_instance_of(described_class)
     end
 
-    it "should raise ArgumentError if model is nil" do
+    it 'raises ArgumentError if model is nil' do
       expect { described_class.new(nil) }.to raise_error(ArgumentError)
     end
   end
 
-  describe "method calls" do
-    let(:adapter){ described_class.new(Fact) }
+  describe 'method calls' do
+    let(:adapter) { described_class.new(Fact) }
 
-    it "should forward calls of missing methods to @connection" do
-      adapter.adapter_name.should == Fact.connection.adapter_name
+    it 'forwards calls of missing methods to @connection' do
+      expect(adapter.adapter_name).to eq(Fact.connection.adapter_name)
     end
 
-    it "should raise NoMethodError error if connection does not respond to missing method" do
+    it 'raises NoMethodError error if connection does not respond to missing method' do
       expect { adapter.foo }.to raise_error(NoMethodError)
     end
   end
